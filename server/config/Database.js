fa9e1364b45a03948aebfa98be87e340ccab08e6
Linnxw <ulin230606@gmail.com>
2023-08-20 19:22:59 +0700

    fixed type in user db

--- server/config/Database.js
+++ server/config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
 const DATABASE_NAME=process.env.DATABASE_NAME || 'sosmed'
-const DATABASE_USER=process.env.DATABAE_USER || 'root'
+const DATABASE_USER=process.env.DATABASE_USER || 'root'
 const DATABASE_PW=process.env.DATABASE_PW || 'root'
 const DATABASE_HOST=process.env.DATABASE_NAME || 'localhost'
 
