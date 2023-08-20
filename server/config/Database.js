aed491890d9719beeba812696b3e399fd69aa995
Linnxw <ulin230606@gmail.com>
2023-08-20 19:25:18 +0700

    fixed type 3

--- server/config/Database.js
+++ server/config/Database.js
@@ -3,7 +3,7 @@ import {Sequelize} from "sequelize";
 const DATABASE_NAME=process.env.DATABASE_NAME || 'sosmed'
 const DATABASE_USER=process.env.DATABASE_USER || 'root'
 const DATABASE_PW=process.env.DATABASE_PW || 'root'
-const DATABASE_HOST=process.env.DATABASE_NAME || 'localhost'
+const DATABASE_HOST=process.env.DATABASE_HOST || 'localhost'
 
 const db=new Sequelize(DATABASE_NAME,DATABASE_USER,DATABASE_PW,{
   host:DATABASE_HOST,
